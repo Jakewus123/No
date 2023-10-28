cee04d45feaba5e91665da3b05460f2f29eac326
Jakewus123 <147282448+Jakewus123@users.noreply.github.com>
2023-10-28 10:00:26 +0800

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,37 @@
+#Writter by Jake wu
+#-----------Import----------#
+Import os
+from os import system as clr
+#------------logo----------#
+ logo=(
+_____            __                 
+   /     |          /  |                
+   $$$$$ |  ______  $$ |   __   ______  
+      $$ | /      \ $$ |  /  | /      \ 
+ __   $$ | $$$$$$  |$$ |_/$$/ /$$$$$$  |
+/  |  $$ | /    $$ |$$   $$<  $$    $$ |
+$$ \__$$ |/$$$$$$$ |$$$$$$  \ $$$$$$$$/ 
+$$    $$/ $$    $$ |$$ | $$  |$$       |
+ $$$$$$/   $$$$$$$/ $$/   $$/  $$$$$$$/ '')
+ #---------linex def--------#
+ def linex():
+#---------clear def--------#
+def clear():
+	clr('clear')
+	print (logo)
+#----------main def--------#
+def JAKE():
+	clear():
+		print(f'{B} [{warna}01{B}] Random cloning ')
+		print(f'{B} [{warna}00{B}] Exit ')
+		option=input(f'{B}[{warna}??{B}] Choise menu')
+		 if option in ('01','1')
+		 BD_CLONING()
+		else:
+			exit('testing only')
+#----------bd clone def---------#
+def BD_CLONINT():
+	print('
+	exit()
+#----------end----------#
+Jake()
